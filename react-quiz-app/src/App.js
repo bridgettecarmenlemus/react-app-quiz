d757ab5174dbff67b3ad64a296a8f3686ac4dc05
Bridgette Lemus <bridgette.lemus.se@gmail.com>
2022-11-09 20:37:38 -0500

    created 4 questions

--- react-quiz-app/src/App.js
+++ react-quiz-app/src/App.js
@@ -18,6 +18,24 @@ export default function App () {
         { answerText: 'git status', isCorrect: false },
         { answerText: 'git clone', isCorrect: false },
         { answerText: 'git init', isCorrect: true },
+      ],
+    },
+    {
+      textQuestion: 'What command would you rub in the terminal to install express and cors?',
+      answerOptions: [
+        { answerText: 'npx create express cors', isCorrect: false },
+        { answerText: 'npm init -y', isCorrect: false },
+        { answerText: 'firebase deploy', isCorrect: false },
+        { answerText: 'npm i express cors', isCorrect: true },
+      ],
+    },
+    {
+      textQuestion: 'Which of the following routes would you most likely expect to return all of the fruits?',
+      answerOptions: [
+        { answerText: "app.create('/fruits', createFruits)", isCorrect: false },
+        { answerText: "app.post('/fruits', createFruits)", isCorrect: false },
+        { answerText: "app.get('/fruits', getAllFruits)", isCorrect: false },
+        { answerText: "app.add('/fruits', getAllFruits)", isCorrect: true },
 
       ]
     }
